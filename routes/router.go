338b066e8a4225c03cc36c35b5037bb378b21bf3
oceanweave <495160589@qq.com>
2022-02-21 19:15:15 +0800

    day3.搭建路由框架，编写响应码，构建路由组v1及相应的处理函数框架

--- routes/router.go
+++ routes/router.go
@@ -1,23 +1,26 @@
 package routes
 
 import (
+	v1 "ginblog/api/v1"
 	"ginblog/utils"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("api/v1")
+	routerV1 := r.Group("api/v1")
 	// 下面的第一个大括号没什么作用，就是为了美观
 	{
-		router.GET("hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "ok",
-			})
-		})
+		// 用户模块的接口
+		routerV1.POST("user/add", v1.AddUser)
+		routerV1.GET("users", v1.GetUsers)
+		routerV1.PUT("user/:id", v1.EditUser)
+		routerV1.DELETE("user/:id", v1.DeleteUser)
+		// 文章模块的接口
+
+		// 分类模块的接口
 	}
 
 	r.Run(utils.HttpPort)
---
+++ utils/errmsg/errmsg.go
@@ -0,0 +1,33 @@
+package errmsg
+
+const (
+	SUCCES = 200
+	ERROR  = 500
+	// code = 1000... 用户模块的错误
+	ERROR_USERNAME_USED    = 1001
+	ERROR_PASSWORD_WRONG   = 1002
+	ERROR_USER_NOT_EXIST   = 1003
+	ERROR_TOKEN_EXIST      = 1004
+	ERROR_TOKEN_RUNTIME    = 1005
+	ERROR_TOKEN_WRONG      = 1006
+	ERROR_TOKEN_TYPE_WRONG = 1007
+	// code = 2000... 文章模块的错误
+
+	// code = 3000... 分类模块的错误
+)
+
+var codeMsg = map[int]string{
+	SUCCES:                 "0K",
+	ERROR:                  "FAIL",
+	ERROR_USERNAME_USED:    "用户名已存在",
+	ERROR_PASSWORD_WRONG:   "密码错误",
+	ERROR_USER_NOT_EXIST:   "用户不存在",
+	ERROR_TOKEN_EXIST:      "TOKEN 不存在",
+	ERROR_TOKEN_RUNTIME:    "TOKEN 已过期",
+	ERROR_TOKEN_WRONG:      "TOKEN 不正确",
+	ERROR_TOKEN_TYPE_WRONG: "TOKEN 格式错误",
+}
+
+func GetErrMsg(code int) string {
+	return codeMsg[code]
+}
